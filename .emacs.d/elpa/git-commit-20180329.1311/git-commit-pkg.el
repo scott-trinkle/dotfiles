021c5a78aaf0fcfbdbfc8814c9df5b1a77aabe0f
Scott Trinkle <tscott.trinkle@gmail.com>
2018-04-01 16:47:11 -0500

    Removes elpa, updates gitignore

--- .emacs.d/elpa/git-commit-20180329.1311/git-commit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-commit" "20180329.1311" "Edit Git commit messages" '((emacs "24.4") (dash "20170810") (with-editor "20180318")) :commit "bde310440fbe97038b1dac4a98997894c30bea4f" :keywords '("git" "tools" "vc") :url "https://github.com/magit/magit")
