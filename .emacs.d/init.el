b141375e2a0fd291e3a341c4cf7b74cb6c944d1b
Scott Trinkle <tscott.trinkle@gmail.com>
2018-04-02 00:09:29 -0500

    Adds various things to make emacs run on siraf

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,3 +3,17 @@
 (require 'org)
 (require 'ob-tangle)
 (org-babel-load-file (expand-file-name "~/.emacs.d/scott.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (magit use-package py-autopep8 fill-column-indicator elpy diminish company-irony auto-complete))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
